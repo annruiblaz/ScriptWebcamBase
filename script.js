d98237b3641495c856962f52283f7003c6e79c9c
annruiblaz <anruiblaz@gmail.com>
2025-02-05 19:23:48 +0100

    base html + script

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
-//Almacenamos una referencia al elemento HTML de video con su id
-const video = document.getElementById('videoElement');
+'use strict'; //Nos ayuda a q el código sea + seguro (Se asegura d q todas las variables q utilizamos han sido previmiante declaradas)
 
-//Verifica si el navegador es compatible con la API de getUserMedia
-if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    //Si es compatible, solicita el acceso a la webcam
-    navigator.mediaDevices.getUserMedia({video: true})
-        .then( function(stream) {
-            //Si el user nos permite acceso a la webcam
-            //Asignamos en el elemento HTML del video el stream (flujo) q nos viene de la webcam
-            video.srcObject = stream;
-        }).catch( function(error) {
-            //En caso de error lo recogemos e imprimimos en consola
-            console.error('Se ha producido un error al acceder a la webcam. ', error);
-        });
-} else {
-    console.error('getUserMedia no es compatible con este navegador.')
+//Definimos las referencias de los HTMLElement q vamos a utilizar
+const videoFrame = document.getElementById('video_frame');
+const canvasFrame = document.getElementById('canvas_frame');
+const snapFrame = document.getElementById('snap_frame');
+const errorMsgElement = document.querySelector('span#errorMsg');
+
+//Establecemos el tamaño del video y si queremos incluir el audio o no
+const constraints = {
+    video: {
+        width: 500,
+        height: 350
+    }
+};
+
+//Comprobamos acceso a webcam
+async function init() {
+    try {
+        const stream = await navigator.mediaDevices.getUserMedia('constraints');
+        handleSuccess(stream);
+    } catch (e) {
+        errorMsgElement.innerHTML = `navigator.getUserMedia error :${e.toString()}`;
+    }
 }
+
+//En caso de que el user acepte los permisos, cargamos la webcam
+async function handleSuccess(stream) {
+    //Guardamos el contexto del canvas para hacer la captura
+    videoFrame.srcObject = stream;
+    //Mostramos la img que hemos capturado (el frame del video)
+}
+
+//Llamamos al método para iniciar el proceso
+init();
